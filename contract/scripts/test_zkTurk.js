eee20bbaa87598a7521c1468204e1f55f12b93b2
Serg Miller <sergey.miller@phystech.edu>
2023-05-13 10:37:13 +0100

    minor fix tests for contract

--- contract/scripts/test_zkTurk.js
+++ contract/scripts/test_zkTurk.js
@@ -23,11 +23,14 @@ describe("ZkTurkContract", function () {
     DEF_ANS_MAX= 2
 
     let contract = null
+    let defaultWorker = null
 
     beforeEach(async () => {
         const Storage = await ethers.getContractFactory("ZkTurkContract");
         contract = await Storage.deploy(DEF_FEE, DEF_FEE);
         contract.deployed();
+        const [, worker1] = await ethers.getSigners();
+        defaultWorker = worker1
         // TODO: to work with 0 in mapping of "workerToProblem";
         await addDefaultProblem()
     });
@@ -50,7 +53,7 @@ describe("ZkTurkContract", function () {
     describe("#addProblem", () => {
         it("it adds problem successfully", async function () {
             // const provider = ethers.getDefaultProvider();
-            const [owner, addr1] = await ethers.getSigners();
+            const [owner, ] = await ethers.getSigners();
             // console.log('here');
             // console.log(owner.address);
             // const balance = await provider.getBalance(owner.address);
@@ -62,21 +65,31 @@ describe("ZkTurkContract", function () {
         })
     })
 
+    async function _joinDefaultProblem() {
+        const options = {value: DEF_FEE}
+        await contract.connect(defaultWorker).joinProblem(1, 'worldIdhash', options)
+    }
+
     describe("#joinProblem", () => {
         it("it joins successfully", async function () {
             await addDefaultProblem()
-            const [, account1] = await ethers.getSigners();
-            const options = {value: DEF_FEE}
-            await contract.connect(account1).joinProblem(1, 'worldIdhash', options)
+            await _joinDefaultProblem()
+            // TODO: test.
             // expect(problem[0]).to.equal(owner.address);
         })
 
         it("it do not joined second time", async function () {
             await addDefaultProblem()
-            const [, account1] = await ethers.getSigners();
-            const options = {value: DEF_FEE}
-            await contract.connect(account1).joinProblem(1, 'worldIdhash', options)
-            await shouldThrow(contract.connect(account1).joinProblem(1, 'worldIdhash', options))
+            await _joinDefaultProblem()
+            await shouldThrow(_joinDefaultProblem())
+        })
+    })
+
+    describe("#solveTask", () => {
+        it("it solve task successfully", async function () {
+            await addDefaultProblem()
+            await _joinDefaultProblem()
+            await contract.connect(defaultWorker).solveTask(1, 0, "cipheredAnswer")
         })
     })
 });
